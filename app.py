7980039de6ccc60bc01165ce55ee964232b6c65b
Tiago Costa <tiagodpv@gmail.com>
2023-09-02 23:30:00 -0400

    final version

--- app.py
+++ app.py
@@ -28,16 +28,12 @@ def obter_livros():
     return jsonify(livros)
 
 # Consultar(id)
-
-
 @app.route('/livros/<int:id>', methods=['GET'])
 def obter_livro_por_id(id):
     for livro in livros:
         if livro.get('id') == id:
             return jsonify(livro)
 # Editar
-
-
 @app.route('/livros/<int:id>', methods=['PUT'])
 def editar_livro_por_id(id):
     livro_alterado = request.get_json()
@@ -45,18 +41,18 @@ def editar_livro_por_id(id):
         if livro.get('id') == id:
             livros[indice].update(livro_alterado)
             return jsonify(livros[indice])
+
+
 # Criar
-
-
 @app.route('/livros', methods=['POST'])
 def incluir_novo_livro():
     novo_livro = request.get_json()
     livros.append(novo_livro)
 
     return jsonify(livros)
+
+
 # Excluir
-
-
 @app.route('/livros/<int:id>', methods=['DELETE'])
 def excluir_livro(id):
     for indice, livro in enumerate(livros):
